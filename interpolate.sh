a3215ffb10f48a379541fd7a71f8a98de841eb74
Michael Davis <michael.davis@solace.com>
2018-01-16 22:49:06 +0000

    Added local-vars, bosh-lite ops file.

---
+++ interpolate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# bosh -d solace_messaging run-errand delete-all
+
+bosh interpolate solace-deployment.yml \
+	-o operations/plan_inventory.yml \
+	-o operations/bosh-lite.yml \
+	-v system_domain=bosh-lite.com  \
+	-v cf_deployment=cf  \
+	-v vmr_edition=evaluation \
+	-l vars.yml \
+	-l local-vars.yml
+
+
