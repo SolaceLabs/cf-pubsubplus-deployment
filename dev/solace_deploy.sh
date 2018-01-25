b6d0800be0f9f557a17db31d88074d5fd3bccdbe
Philippe Khalife <philippe.khalife@gmail.com>
2018-01-25 14:54:20 -0500

    Use tls config, trusted

--- dev/solace_deploy.sh
+++ dev/solace_deploy.sh
@@ -19,6 +19,10 @@ WORKSPACE=${WORKSPACE:-$SCRIPTPATH/../workspace}
 export BOSH_NON_INTERACTIVE=${BOSH_NON_INTERACTIVE:-true}
 export VMR_EDITION=${VMR_EDITION:-"evaluation"}
 
+if [ -f $WORKSPACE/bosh_env.sh ]; then
+ source $WORKSPACE/bosh_env.sh
+fi
+
 cd $SCRIPTPATH/..
 
 bosh -d solace_messaging \
@@ -26,15 +30,18 @@ bosh -d solace_messaging \
 	-o operations/set_plan_inventory.yml \
 	-o operations/bosh_lite.yml \
 	-o operations/is_${VMR_EDITION}.yml \
+	-o operations/config_tls.yml \
 	-o operations/enable_global_access_to_plans.yml \
 	--vars-store $WORKSPACE/deployment-vars.yml \
 	-v system_domain=bosh-lite.com  \
 	-v app_domain=bosh-lite.com  \
 	-v cf_deployment=cf  \
 	-l vars.yml \
-	-l release-vars.yml 
+	-l release-vars.yml \
+        -l operations/example-vars-files/certs.yml 
 
-[[ $? ]] && { 
+
+[[ $? -eq 0 ]] && { 
   $SCRIPTPATH/solace_add_service_broker.sh 
 }
 
