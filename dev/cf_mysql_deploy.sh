370fd6f53a904af36cbdf37c70206e3a9d639728
Philippe Khalife <philippe.khalife@gmail.com>
2018-01-25 13:17:57 -0500

    Ensure access to bosh env

--- dev/cf_mysql_deploy.sh
+++ dev/cf_mysql_deploy.sh
@@ -18,7 +18,15 @@ WORKSPACE=${WORKSPACE:-$SCRIPTPATH/../workspace}
 
 export BOSH_NON_INTERACTIVE=${BOSH_NON_INTERACTIVE:-true}
 
+if [ -f $SCRIPTPATH/cf_env.sh ]; then
+ $SCRIPTPATH/cf_env.sh
+fi
+
 cd $WORKSPACE
+
+if [ -f bosh_env.sh ]; then
+ source bosh_env.sh
+fi
 
 if [ ! -d cf-mysql-deployment ]; then
   git clone https://github.com/cloudfoundry/cf-mysql-deployment.git
@@ -36,11 +44,15 @@ bosh \
   -l $SCRIPTPATH/cf_mysql_vars.yml \
   --vars-store $WORKSPACE/deployment-vars.yml
 
-bosh -d cf-mysql run-errand broker-registrar-vm
+[[ $? -eq 0 ]] && {
+  bosh -d cf-mysql run-errand broker-registrar-vm
+}
 
-echo "Create a test org and space to check marketplace"
-cf create-org test
-cf target -o test
-cf create-space test
-cf target -o test
-cf m -s p-mysql
+[[ $? -eq 0  ]] && {
+ echo "Create a test org and space to check marketplace"
+ cf create-org test
+ cf target -o test
+ cf create-space test
+ cf target -o test
+ cf m -s p-mysql
+}
