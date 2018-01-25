6672b89652cc2f8deaaf41894e56b2ec59acdbcb
Philippe Khalife <philippe.khalife@gmail.com>
2018-01-25 13:21:59 -0500

    Ensure access to bosh env, support VMR_EDITION selection, use TLS

--- dev/solace_interpolate.sh
+++ dev/solace_interpolate.sh
@@ -17,18 +17,25 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 WORKSPACE=${WORKSPACE:-$SCRIPTPATH/../workspace}
 
 export BOSH_NON_INTERACTIVE=${BOSH_NON_INTERACTIVE:-true}
+export VMR_EDITION=${VMR_EDITION:-"evaluation"}
+
+if [ -f $WORKSPACE/bosh_env.sh ]; then
+ source $WORKSPACE/bosh_env.sh
+fi
 
 cd $SCRIPTPATH/..
 
 bosh interpolate solace-deployment.yml \
-	-o operations/set_plan_inventory.yml \
-	-o operations/bosh_lite.yml \
-	-o operations/is_evaluation.yml \
-	-o operations/enable_global_access_to_plans.yml \
-	-v system_domain=bosh-lite.com  \
-	-v app_domain=bosh-lite.com  \
-	-v cf_deployment=cf  \
-	-l vars.yml \
-	-l release-vars.yml
-
+        -o operations/set_plan_inventory.yml \
+        -o operations/bosh_lite.yml \
+        -o operations/config_tls.yml \
+        -o operations/is_${VMR_EDITION}.yml \
+        -o operations/enable_global_access_to_plans.yml \
+        --vars-store $WORKSPACE/deployment-vars.yml \
+        -v system_domain=bosh-lite.com  \
+        -v app_domain=bosh-lite.com  \
+        -v cf_deployment=cf  \
+        -l vars.yml \
+        -l release-vars.yml \
+        -l operations/example-vars-files/certs.yml
 
