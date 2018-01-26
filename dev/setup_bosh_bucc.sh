689ede9fc4150ff0fd013f8a105ae3dd3de37dee
Philippe Khalife <philippe.khalife@gmail.com>
2018-01-25 19:08:17 -0500

    Removing readlink use

--- dev/setup_bosh_bucc.sh
+++ dev/setup_bosh_bucc.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-# Cross-OS compatibility ( greadlink, gsed )
-[[ `uname` == 'Darwin' ]] && {
-	which greadlink gsed > /dev/null || {
-		echo 'ERROR: GNU utils required for Mac. You may use homebrew to install them: brew install coreutils gnu-sed'
-		exit 1
-	}
-
-   shopt -s expand_aliases
-   alias readlink=`which greadlink`
-   alias sed=`which gsed`
-}
-
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-WORKSPACE=${WORKSPACE:-$SCRIPTPATH/../workspace}
+export SCRIPT="$( basename "${BASH_SOURCE[0]}" )"
+export SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export WORKSPACE=${WORKSPACE:-$SCRIPTPATH/../workspace}
 
 export BOSH_NON_INTERACTIVE=${BOSH_NON_INTERACTIVE:-true}
 
