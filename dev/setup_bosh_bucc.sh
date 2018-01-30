f45a7863b1a43cc2acb435ecaf9f861f5e3c0643
Philippe Khalife <philippe.khalife@gmail.com>
2018-01-30 10:02:40 -0500

    Use common script to handle cross os concerns

--- dev/setup_bosh_bucc.sh
+++ dev/setup_bosh_bucc.sh
@@ -4,6 +4,10 @@ export SCRIPT="$( basename "${BASH_SOURCE[0]}" )"
 export SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export WORKSPACE=${WORKSPACE:-$SCRIPTPATH/../workspace}
 
+source $SCRIPTPATH/common.sh
+
+export VM_MEMORY=${VM_MEMORY:-8192}
+
 export BOSH_NON_INTERACTIVE=${BOSH_NON_INTERACTIVE:-true}
 
 if [ ! -d $WORKSPACE ]; then
@@ -16,7 +20,8 @@ if [ ! -d bucc ]; then
  git clone https://github.com/starkandwayne/bucc.git
 fi
 
-sed -i 's/vm_memory: 4096/vm_memory: 8192/' $WORKSPACE/bucc/ops/cpis/virtualbox/vars.tmpl
+echo "Setting VM MEMORY to $VM_MEMORY"
+sed -i "s/vm_memory: 4096/vm_memory: $VM_MEMORY/" $WORKSPACE/bucc/ops/cpis/virtualbox/vars.tmpl
 
 $WORKSPACE/bucc/bin/bucc up --cpi virtualbox --lite --debug | tee $WORKSPACE/bucc_up.log
 $WORKSPACE/bucc/bin/bucc env > $WORKSPACE/bosh_env.sh
