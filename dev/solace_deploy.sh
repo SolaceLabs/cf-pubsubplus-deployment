b482e8c9e73f84cfbcad20a63179c85670ebea0e
natmay1 <natasha.may.wall@gmail.com>
2018-02-21 18:20:26 +0000

    local changes before merge

--- dev/solace_deploy.sh
+++ dev/solace_deploy.sh
@@ -4,8 +4,8 @@ export SCRIPT="$( basename "${BASH_SOURCE[0]}" )"
 export SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export WORKSPACE=${WORKSPACE:-$SCRIPTPATH/../workspace}
 
-export VARS_PATH=$SCRIPTPATH/vars.yml
-export TLS_PATH=$SCRIPTPATH/operations/example-vars-files/certs.yml
+export VARS_PATH=$SCRIPTPATH/../vars.yml
+export TLS_PATH=$SCRIPTPATH/../operations/example-vars-files/certs.yml
 
 export BOSH_NON_INTERACTIVE=${BOSH_NON_INTERACTIVE:-true}
 export VMR_EDITION=${VMR_EDITION:-"evaluation"}
@@ -23,15 +23,17 @@ function showUsage() {
     echo "  -p <vmr_admin_password>   provide vmr admin password "
     echo "  -h                        show command options "
     echo "  -v <vars.yml file path>   provide vars.yml file path "
-    echo "  -t <tls_config.yml file>  provide tls config file path"
+    echo "  -l <tls_config.yml file>  provide tls config file path"
+    echo "  -e                        is enterprise mode"
     echo "$WORKSPACE"
 }
 
 
-while getopts "t:s:p:v:h" arg; do
+while getopts "l:s:p:v:eh" arg; do
     case "${arg}" in
-        t) 
+        l) 
             export TLS_PATH="$OPTARG"
+            shift
             ;;
         s)
             starting_port="$OPTARG"
@@ -42,9 +44,14 @@ while getopts "t:s:p:v:h" arg; do
             vmr_admin_password="${OPTARG}"
             grep -q 'vmr_admin_password' vars.yml && sed -i "s/vmr_admin_password.*/vmr_admin_password: $vmr_admin_password/" vars.yml || echo "vmr_admin_password: $vmr_admin_password" >> vars.yml
             ;;
-        v) 
-            export VARS_PATH="$OPTARG" 
+        v)
+            export VARS_PATH="$SCRIPTPATH/$OPTARG"
+            shift
+            echo $SCRIPTPATH
             ;; 
+        e) 
+	    export VMR_EDITION="enterprise"
+            ;;
         h)
             showUsage
             exit 0
@@ -84,7 +91,7 @@ bosh -d solace_messaging \
 	-o operations/set_solace_vmr_cert.yml \
 	-o operations/enable_global_access_to_plans.yml \
 	-o operations/is_${VMR_EDITION}.yml \
-	--vars-store $WORKSPACE/deployment-vars.yml \
+	--vars-store $SCRIPTPATH/deployment-vars.yml \
 	-v system_domain=bosh-lite.com  \
 	-v app_domain=bosh-lite.com  \
 	-v cf_deployment=cf  \
