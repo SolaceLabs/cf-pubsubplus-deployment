3cc1ccb1cf92f0931b97539ac5ed660f046ea5b4
Philippe Khalife <philippe.khalife@gmail.com>
2018-03-01 10:27:51 -0500

    Always update bucc, more details for adding routes and swap

--- dev/setup_bosh_bucc.sh
+++ dev/setup_bosh_bucc.sh
@@ -19,6 +19,8 @@ cd $WORKSPACE
 
 if [ ! -d bucc ]; then
  git clone https://github.com/starkandwayne/bucc.git
+else
+ (cd bucc; git pull)
 fi
 
 echo "Setting VM MEMORY to $VM_MEMORY, VM_CPUS to $VM_CPUS"
@@ -28,7 +30,12 @@ sed -i "s/vm_cpus: 2/vm_cpus: $VM_CPUS/" $WORKSPACE/bucc/ops/cpis/virtualbox/var
 $WORKSPACE/bucc/bin/bucc up --cpi virtualbox --lite --debug | tee $WORKSPACE/bucc_up.log
 $WORKSPACE/bucc/bin/bucc env > $WORKSPACE/bosh_env.sh
 
-echo "Adding routes, you may need to enter credentials"
+echo
+echo "Adding swap. Please accept the The authenticity of host '192.168.50.6' when requested"
+echo
 $SCRIPTPATH/setup_bosh_swap.sh
+echo
+echo "Adding routes, you may need to enter your credentials to grant sudo permissions"
+echo
 $SCRIPTPATH/setup_bosh_routes.sh
 
