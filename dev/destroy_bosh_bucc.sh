baba121e0599f55b6f42d79da2028bd8739fd28c
Philippe Khalife <philippe.khalife@gmail.com>
2018-03-01 10:36:55 -0500

    Destroys bosh setup from bucc

---
+++ dev/destroy_bosh_bucc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export SCRIPT="$( basename "${BASH_SOURCE[0]}" )"
+export SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export WORKSPACE=${WORKSPACE:-$SCRIPTPATH/../workspace}
+
+source $SCRIPTPATH/common.sh
+
+export BOSH_NON_INTERACTIVE=${BOSH_NON_INTERACTIVE:-true}
+
+if [ ! -d $WORKSPACE ]; then
+  mkdir -p $WORKSPACE
+fi
+
+cd $WORKSPACE
+
+if [ ! -d bucc ]; then
+ git clone https://github.com/starkandwayne/bucc.git
+else
+ (cd bucc; git pull)
+fi
+
+$WORKSPACE/bucc/bin/bucc down 
+$WORKSPACE/bucc/bin/bucc clean
+
