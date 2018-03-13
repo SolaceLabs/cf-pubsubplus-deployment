a9ad0bfb7747175e350dd04548fcc200b27b5ed4
Philippe Khalife <philippe.khalife@gmail.com>
2018-03-13 14:53:13 -0400

    Allow setting disk size: VM_DISK_SIZE, defaults to 64GB

--- dev/setup_bosh_bucc.sh
+++ dev/setup_bosh_bucc.sh
@@ -8,6 +8,7 @@ source $SCRIPTPATH/common.sh
 
 export VM_MEMORY=${VM_MEMORY:-8192}
 export VM_CPUS=${VM_CPUS:-4}
+export VM_DISK_SIZE=${VM_DISK_SIZE:-"65_536"}
 
 export BOSH_NON_INTERACTIVE=${BOSH_NON_INTERACTIVE:-true}
 
@@ -26,6 +27,8 @@ fi
 echo "Setting VM MEMORY to $VM_MEMORY, VM_CPUS to $VM_CPUS"
 sed -i "s/vm_memory: 4096/vm_memory: $VM_MEMORY/" $WORKSPACE/bucc/ops/cpis/virtualbox/vars.tmpl
 sed -i "s/vm_cpus: 2/vm_cpus: $VM_CPUS/" $WORKSPACE/bucc/ops/cpis/virtualbox/vars.tmpl
+echo "vm_disk_size: $VM_DISK_SIZE" >> $WORKSPACE/bucc/ops/cpis/virtualbox/vars.tmpl
+cp -f $SCRIPTPATH/vm-size.yml $WORKSPACE/bucc/ops/cpis/virtualbox/
 
 $WORKSPACE/bucc/bin/bucc up --cpi virtualbox --lite --debug | tee $WORKSPACE/bucc_up.log
 $WORKSPACE/bucc/bin/bucc env > $WORKSPACE/bosh_env.sh
